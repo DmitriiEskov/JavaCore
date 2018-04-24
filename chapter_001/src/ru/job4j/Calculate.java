5844ff6c0f766a7776217bb5b6420f183b65aefa
DmitriiEskov <dmitryhope@yahoo.com>
2018-04-24 23:20:05 +0300

    1.3. JavaDoc. Code convetion.

--- chapter_001/src/ru/job4j/Calculate.java
+++ chapter_001/src/ru/job4j/Calculate.java
@@ -1,6 +1,15 @@
 package ru.job4j;
 
+/**
+ * Calculate
+ * 
+ * @author Dmitrii Eskov (dmitryhope@yahoo.com)
+ */
 public class Calculate {
+	/**
+	 * Main.
+	 * @param args - args.
+	 */
 	public static void main(String[] args){
 		System.out.println("Hello world");		
 	}
