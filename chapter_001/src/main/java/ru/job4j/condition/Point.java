64db776a9e036b85267f7455d3140025d8a1a133
Dmitrii Eskov <dmitryhope@yahoo.com>
2018-06-08 14:28:41 +0300

    3.4. Distance between two points in the coordinate system.

---
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -0,0 +1,42 @@
+package ru.job4j.condition;
+
+/**
+ * @author Dmitrii Eskov (dmitryhope@yahoo.com)
+ * @version 1.0
+ * @since 07.06.2018
+ */
+public class Point {
+    private int x;
+    private int y;
+
+    /**
+     * Constructor.
+     * @param x - position for .x
+     * @param y - position for .y
+     */
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * Distance between .A and .B
+     * @param that - x and y for Point that
+     * @return distance between .A and .B
+     */
+    public double distanceTo(Point that) {
+        return Math.sqrt(
+                Math.pow(this.x - that.x, 2) + Math.pow(this.y - that.y, 2)
+        );
+    }
+
+    public static void main(String[] arqs) {
+        Point a = new Point(10, 10);
+        Point b = new Point(5, 5);
+        double result = a.distanceTo(b);
+        // Расстояние точки А между собой
+        double secondResult = a.distanceTo(a);
+        System.out.println("Расстояние между точками А и Б равно: " + result);
+        System.out.println("Расстояние точки А между собой: " + secondResult);
+    }
+}
