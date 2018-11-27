2dab92e4f56d43a4ba994c7f7c9ce28ca46bce15
DmitriiEskov <dmitryhope@yahoo.com>
2018-11-27 14:04:45 +0300

    Puzzle game
    Puzzle game
    
    Puzzle game
    
    Puzzle game
    
    Puzzle game

---
+++ chapter_001/src/main/java/ru/job4j/puzzle/Logic.java
@@ -0,0 +1,101 @@
+package ru.job4j.puzzle;
+
+import ru.job4j.puzzle.firuges.Cell;
+import ru.job4j.puzzle.firuges.Figure;
+
+/**
+ * Class for logical processes of "Puzzle" game.
+ *
+ * @author Dmitrii Eskov (eskovdmi@gmail.com)
+ * @version $Id$
+ * @since 27.11.2018
+ */
+public class Logic {
+    private final int size;
+    private final Figure[] figures;
+    private int index = 0;
+
+    public Logic(int size) {
+        this.size = size;
+        this.figures = new Figure[size * size];
+    }
+
+    public void add(Figure figure) {
+        this.figures[this.index++] = figure;
+    }
+
+    public boolean move(Cell source, Cell dest) {
+        boolean rst = false;
+        int index = this.findBy(source);
+        if (index != -1) {
+            Cell[] steps = this.figures[index].way(source, dest);
+            if (this.isFree(steps)) {
+                rst = true;
+                this.figures[index] = this.figures[index].copy(dest);
+            }
+        }
+        return rst;
+    }
+
+    public boolean isFree(Cell... cells) {
+        boolean result = cells.length > 0;
+        for (Cell cell : cells) {
+            if (this.findBy(cell) != -1) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public void clean() {
+        for (int position = 0; position != this.figures.length; position++) {
+            this.figures[position] = null;
+        }
+        this.index = 0;
+    }
+
+    private int findBy(Cell cell) {
+        int rst = -1;
+        for (int index = 0; index != this.figures.length; index++) {
+            if (this.figures[index] != null && this.figures[index].position().equals(cell)) {
+                rst = index;
+                break;
+            }
+        }
+        return rst;
+    }
+
+    public boolean isWin() {
+        int[][] table = this.convert();
+        boolean result = false;
+        for (int i = 0; i < table.length; i++) {
+            if (table[i][0] == 1 || table[0][i] == 1) {
+                int horizontalSum = table[i][0];
+                int verticalSum = table[0][i];
+                for (int j = 1; j < table.length; j++) {
+                    horizontalSum = horizontalSum + table[i][j];
+                    verticalSum = verticalSum + table[j][i];
+                }
+                if (horizontalSum == table.length || verticalSum == table.length) {
+                    result = true;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+
+    public int[][] convert() {
+        int[][] table = new int[this.size][this.size];
+        for (int row = 0; row != table.length; row++) {
+            for (int cell = 0; cell != table.length; cell++) {
+                int position = this.findBy(new Cell(row, cell));
+                if (position != -1 && this.figures[position].movable()) {
+                    table[row][cell] = 1;
+                }
+            }
+        }
+        return table;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/puzzle/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Test for Logic class.
+ *
+ * @author Dmitrii Eskov (eskovdmi@gmail.com)
+ * @version 1.0
+ * @since 27.11.2018
+ */
+package ru.job4j.puzzle;
