da3456fdc895c6064b79897b1ef87bc59eb30965
DmitriiEskov <dmitryhope@yahoo.com>
2018-12-07 00:25:30 +0300

    Algorithm for creating a sorted array based on the given two sorted arrays.

--- chapter_001/src/main/java/ru/job4j/array/FillArray.java
+++ chapter_001/src/main/java/ru/job4j/array/FillArray.java
@@ -1,41 +1,27 @@
 package ru.job4j.array;
 
 /**
- * Class for filling in an array with sorted values taken from two other sorted arrays WITHOUT sorting values inside the array.
+ * Class for creating a sorted array based on the given two sorted arrays.
  *
  * @author Dmitrii Eskov (eskovdmi@gmail.com)
  * @version 1.0
- * @since 03.12.2018
+ * @since 06.12.2018
  */
 public class FillArray {
 
     /**
-     * Filling in an array with sorted values taken from two sorted arrays in the following way:
-     * 1. Filling in values into the result array from the first one.
-     * 2. Filling in values into the result array from the second one.
+     * Filling in an array with sorted values taken from two other sorted arrays.
+     * This method does not sort itself!
      *
-     * @param first  - the first sorted array
-     * @param second - the second sorted array
+     * @param a  - the first sorted array
+     * @param b - the second sorted array
      * @return result - the array that contains sorted values from both first and second arrays
      */
-    public int[] fill(int[] first, int[] second) {
-        int[] result = new int[first.length + second.length];
-        int count = 0;
-        for (int i = 0; i < first.length; i++) {
-            if (count < second.length && first[i] >= second[count]) {
-                result[i + count] = second[count++];
-                while (count < second.length) {
-                    if (first[i] >= second[count]) {
-                        result[i + count] = second[count++];
-                    } else {
-                        break;
-                    }
-                }
-            }
-            result[i + count] = first[i];
-        }
-        while (count < second.length) {
-            result[first.length + count] = second[count++];
+    public int[] fill(int[] a, int[] b) {
+        int[] result = new int[a.length + b.length];
+        int i = a.length - 1, j = b.length - 1, k = result.length;
+        while (k > 0) {
+            result[--k] = (j < 0 || (i >= 0 && a[i] >= b[j])) ? a[i--] : b[j--];
         }
         return result;
     }
