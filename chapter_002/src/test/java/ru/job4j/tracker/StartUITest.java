18d0b0daae9fd305bc97917eef39be00582cc4cb
DmitriiEskov <dmitryhope@yahoo.com>
2018-12-15 16:35:36 +0300

    4.6. Added 2 tests for the "StartUITest". 1. A test for the "getAll" method. 2. A test for the "findByName" method.

--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,6 +1,11 @@
 package ru.job4j.tracker;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -12,6 +17,16 @@ import static org.junit.Assert.assertThat;
  * @version 1.0
  */
 public class StartUITest {
+
+    /**
+     * The default output to the console.
+     */
+    private final PrintStream stdout = System.out;
+
+    /**
+     * The buffer for a result.
+     */
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
 
     /**
      * Tests when a user wants to add an item.
@@ -73,5 +88,95 @@ public class StartUITest {
         Item[] result = new Item[1];
         result[0] = item;
         assertThat(tracker.findByName(item.getName()), is(result));
+    }
+
+    /**
+     * Reassigns the output stream.
+     */
+    @Before
+    public void loadOutput() {
+        System.setOut(new PrintStream(this.out));
+    }
+
+    /**
+     * Reassigns the output stream back.
+     */
+    @After
+    public void backOutput() {
+        System.setOut(new PrintStream(this.stdout));
+    }
+
+    /**
+     * Tests when it needs all created items to be shown.
+     */
+    @Test
+    public void whenGetAllItems() {
+        Tracker tracker = new Tracker();
+        Item first = tracker.add(new Item("First", "Testing description."));
+        Input input = new StubInput(new String[] {"1", "6"});
+        new StartUI(input, tracker).init();
+        assertThat(new String(this.out.toByteArray()), is(
+                        new StringBuilder()
+                                .append("Menu\r\n")
+                                .append("0. Add new Item.\r\n")
+                                .append("1. Show all items.\r\n")
+                                .append("2. Edit item.\r\n")
+                                .append("3. Delete item.\r\n")
+                                .append("4. Find item by an id.\r\n")
+                                .append("5. Find items by a name.\r\n")
+                                .append("6. Exit Programme.\r\n")
+                                .append("------------ Showing all created items --------------\r\n\r\n")
+                                .append("id: " + first.getId() + "\r\n")
+                                .append("Name: " + first.getName() + "\r\n")
+                                .append("Created: " + first.getCreate() + "\r\n")
+                                .append("Description: " + first.getDescription() + "\r\n" + "\r\n")
+                                .append("Menu" + "\r\n")
+                                .append("0. Add new Item." + "\r\n")
+                                .append("1. Show all items." + "\r\n")
+                                .append("2. Edit item." + "\r\n")
+                                .append("3. Delete item." + "\r\n")
+                                .append("4. Find item by an id." + "\r\n")
+                                .append("5. Find items by a name." + "\r\n")
+                                .append("6. Exit Programme." + "\r\n")
+                                .toString()
+                )
+        );
+    }
+
+    /**
+     * Tests when it needs an item by name to be shown.
+     */
+    @Test
+    public void whenGetItemByName() {
+        Tracker tracker = new Tracker();
+        Item first = tracker.add(new Item("Second", "Testing description."));
+        Input input = new StubInput(new String[] {"5", first.getName(), "6"});
+        new StartUI(input, tracker).init();
+        assertThat(new String(this.out.toByteArray()), is(
+                        new StringBuilder()
+                                .append("Menu\r\n")
+                                .append("0. Add new Item.\r\n")
+                                .append("1. Show all items.\r\n")
+                                .append("2. Edit item.\r\n")
+                                .append("3. Delete item.\r\n")
+                                .append("4. Find item by an id.\r\n")
+                                .append("5. Find items by a name.\r\n")
+                                .append("6. Exit Programme.\r\n")
+                                .append("------------ Success! --------------\r\n")
+                                .append("id: " + first.getId() + "\r\n")
+                                .append("Name: " + first.getName() + "\r\n")
+                                .append("Description: " + first.getDescription() + "\r\n")
+                                .append("Create: " + first.getCreate() + "\r\n" + "\r\n")
+                                .append("Menu" + "\r\n")
+                                .append("0. Add new Item." + "\r\n")
+                                .append("1. Show all items." + "\r\n")
+                                .append("2. Edit item." + "\r\n")
+                                .append("3. Delete item." + "\r\n")
+                                .append("4. Find item by an id." + "\r\n")
+                                .append("5. Find items by a name." + "\r\n")
+                                .append("6. Exit Programme." + "\r\n")
+                                .toString()
+                )
+        );
     }
 }
